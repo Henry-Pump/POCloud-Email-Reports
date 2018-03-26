a462070cf9afcbaf6361bc880ae734adef9f9460
Patrick McDonagh <patrickjmcd@gmail.com>
2018-03-26 09:34:53 -0500

    use packaged Meshify

--- build_lambda.sh
+++ build_lambda.sh
@@ -1,11 +1,11 @@
-!#/bin/bash
+
 env/bin/pip install boto3 tzlocal meshify requests
 rm -f lambda.zip
 mkdir -p deploy
-cp env/lib/python3.6/site-packages/* deploy
-cp meshify.py deploy
+cp -R env/lib/python3.6/site-packages/* deploy
 cp reports_s3_xlsx.py deploy
 
 cd deploy
 zip -r ../lambda.zip ./*
 cd ..
+echo "Lambda zip file built to ./lambda.zip"
